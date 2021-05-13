5148220955d279e8e61e8abf5169c581c7d0258c
realNasirMalik <realNasirMalik@gmail.com>
2021-05-12 19:07:27 -0500

    :hammer: Fix My Contract
    Wrong version on the pragma, constructor was not working.

--- MyContract.sol
+++ MyContract.sol
@@ -1,18 +1,19 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.0 <0.9.0;
 
 // This smart contract can be viewed public on the blockchain.
 contract MyContract {
     string value;
 
-    constructor() public  {
+    constructor() {
         value = "myValue";
     }
 
-    function get() public view returns(string) {
+    function get() public view returns(string memory) {
         return value;
     }
 
-    function set(string _value) public {
+    function set(string memory _value) public {
         value = _value;
     }
 }
