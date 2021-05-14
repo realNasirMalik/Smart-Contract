a95bbbaa351cada109dce59a80c12c8dfde098cb
realNasirMalik <realNasirMalik@gmail.com>
2021-05-14 17:06:18 -0500

    :alarm_clock: Test timed contracts
    Some smart contracts have a time duration, this smart contract is just a timed execution test.

--- MyContract.sol
+++ MyContract.sol
@@ -7,14 +7,31 @@ contract MyContract {
      // uint = key|id; person = pair|record; similar to database
     mapping(uint => Person) public people;
 
+    uint256 openingTime = 1621029702;
+
+    modifier onlyWhileOpen() {
+        require(block.timestamp >= openingTime);
+        _;
+    }
+
     struct Person {
         uint _id;
         string _firstName;
         string _lastName;
     }
 
-    function addPerson(string memory _firstName, string memory _lastName) public {
-        peopleCount += 1;
+    function addPerson(
+        string memory _firstName,
+        string memory _lastName
+    )
+        public
+        onlyWhileOpen
+    {
+        incrementCount();
         people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+
+    function incrementCount() internal {
+        peopleCount += 1;
     }
 }
