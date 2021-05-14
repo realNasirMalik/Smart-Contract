dccc7b4707edad7b955324baea09eb91744cd7b2
realNasirMalik <realNasirMalik@gmail.com>
2021-05-14 18:57:29 -0500

    :page_facing_up: Test minting
    Testing the mint functionality on a ERC20Token. Also testing the usage of events / emits functionality.

--- MyContract.sol
+++ MyContract.sol
@@ -1,13 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.0 <0.9.0;
 
+contract ERC20Token {
+    string public name;
+    mapping(address => uint256) public balances;
+
+    function mint() public {
+        balances[msg.sender]++;
+    }
+}
+
 // This smart contract can be viewed public on the blockchain.
 contract MyContract {
-    mapping(address => uint256) public balances;
     address payable wallet;
+    address public token;
 
-    constructor(address payable _wallet) {
+    // Let's you log events.
+    event Purchase(
+        address indexed _buyer,
+        uint256 _amount
+    );
+
+    constructor(address payable _wallet, address _token) {
         wallet = _wallet;
+        token = _token;
     }
 
     fallback() external payable {
@@ -19,9 +35,9 @@ contract MyContract {
     }
 
     function buyToken() public payable {
-        // buy a Token
-        balances[msg.sender] += 1;
+        ERC20Token(address(token)).mint();
         // send ether to the wallet
         wallet.transfer(msg.value);
+        emit Purchase(msg.sender, 1);
     }
 }
