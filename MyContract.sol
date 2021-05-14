205ba37b47cd4fbf7c0988be0bce7cb413311f3e
realNasirMalik <realNasirMalik@gmail.com>
2021-05-14 18:05:23 -0500

    :moneybag: Test Payable
    This test is to check the transfer of tokens.

--- MyContract.sol
+++ MyContract.sol
@@ -3,35 +3,17 @@ pragma solidity >=0.4.0 <0.9.0;
 
 // This smart contract can be viewed public on the blockchain.
 contract MyContract {
-    uint256 public peopleCount = 0;
-     // uint = key|id; person = pair|record; similar to database
-    mapping(uint => Person) public people;
+    mapping(address => uint256) public balances;
+    address payable wallet;
 
-    uint256 openingTime = 1621029702;
-
-    modifier onlyWhileOpen() {
-        require(block.timestamp >= openingTime);
-        _;
+    constructor(address payable _wallet) {
+        wallet = _wallet;
     }
 
-    struct Person {
-        uint _id;
-        string _firstName;
-        string _lastName;
-    }
-
-    function addPerson(
-        string memory _firstName,
-        string memory _lastName
-    )
-        public
-        onlyWhileOpen
-    {
-        incrementCount();
-        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
-    }
-
-    function incrementCount() internal {
-        peopleCount += 1;
+    function buyToken() public payable {
+        // buy a Token
+        balances[msg.sender] += 1;
+        // send ether to the wallet
+        wallet.transfer(msg.value);
     }
 }
