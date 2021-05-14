be41745ab7a2392a9f6ad4c2bf89dc30b14ee180
realNasirMalik <realNasirMalik@gmail.com>
2021-05-14 16:39:00 -0500

    :apple: Test enum class
    Enum is a class that has a group of constants.

--- MyContract.sol
+++ MyContract.sol
@@ -3,10 +3,19 @@ pragma solidity >=0.4.0 <0.9.0;
 
 // This smart contract can be viewed public on the blockchain.
 contract MyContract {
-    string public stringValue = "myString";
-    bool public myBool = true;
-    int public myInt = -1;
-    uint public myUint = 1;
-    uint8 public myUint8 = 111;
-    uint256 public myUint256 = 99999999999999999999999999999999999999999999999999999999999999999999999999999;
+    // enum is a class
+    enum State {Waiting, Ready, Active} // 0, 1, 2
+    State public state; // gives a public access
+
+    constructor() {
+        state = State.Ready;
+    }
+
+    function activate() public {
+        state = State.Active;
+    }
+
+    function isActive() public view returns(bool) {
+        return state == State.Active;
+    }
 }
