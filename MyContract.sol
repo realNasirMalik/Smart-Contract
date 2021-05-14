2112feb7db002c4712222c66b29c77451ade5a63
realNasirMalik <realNasirMalik@gmail.com>
2021-05-14 16:09:32 -0500

    ⚡️Test Data Types
    Testing different types of data types in Solidity. It seems like unsigned integers are important when it comes to created financial related smart contracts.

--- MyContract.sol
+++ MyContract.sol
@@ -3,13 +3,10 @@ pragma solidity >=0.4.0 <0.9.0;
 
 // This smart contract can be viewed public on the blockchain.
 contract MyContract {
-    string public value;
-
-    constructor() {
-        value = "myValue";
-    }
-
-    function set(string memory _value) public {
-        value = _value;
-    }
+    string public stringValue = "myString";
+    bool public myBool = true;
+    int public myInt = -1;
+    uint public myUint = 1;
+    uint8 public myUint8 = 111;
+    uint256 public myUint256 = 99999999999999999999999999999999999999999999999999999999999999999999999999999;
 }
