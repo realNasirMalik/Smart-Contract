e9286819b6ba3a3110b4af0e054148ad45c89c9a
realNasirMalik <realNasirMalik@gmail.com>
2021-05-14 20:00:52 -0500

    :monocle_face: Test SafeMath
    SafeMath is a libarary developed by  OpenZeppelin.

--- MyContract.sol
+++ MyContract.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.0 <0.9.0;
 
-import "./Math.sol";
+import "./SafeMath.sol";
 
 contract MyContract {
+    using SafeMath for uint256;
     uint256 public value;
 
     function calculate(uint _value1, uint _value2) public {
-        Math.divide(_value1, _value2);
-        value = _value1 / _value2;
+        value = _value1.div(_value2);
     }
 }
